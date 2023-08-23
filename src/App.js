8dd91cebd43e628d61dd5307e37fc4547f66b8cc
vishrutss <vishrutss@gmail.com>
2023-08-23 16:45:31 +0530

    Added all component files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { CssBaseline, Grid } from "@mui/material";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
-    return (
-        <div>
-            <h1>My React App!</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default App;
